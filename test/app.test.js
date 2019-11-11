aad56aeeaa1578ce12a82444d00d20011064cfd4
tarajeanne <t.jeanne.patel@gmail.com>
2019-11-10 22:23:16 -0600

    removed .only from a test

--- test/app.test.js
+++ test/app.test.js
@@ -1,5 +1,5 @@
 const app = require('../src/app');
-describe('server response with 200 and Hello world',()=>{
+describe('server response with 200',()=>{
   it('resonse with a 200 code',()=>{
     return supertest(app).get('/').expect(200);
   });
